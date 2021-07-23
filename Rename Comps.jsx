8699e89ecd51c9eadafbac5c91ee9147b6f96e52
Gregory Wakefield <gregory.k.wakefield@gmail.com>
2021-07-23 15:14:38 +0100

    Update UI

--- Rename Comps.jsx
+++ Rename Comps.jsx
@@ -1,79 +1,77 @@
 ﻿var panelGlobal = this;
+var palette = (function () {
 
-// COMPRENAMER
-// ===========
-var compReNamer = (panelGlobal instanceof Panel) ? panelGlobal : new Window("palette"); 
-    if ( !(panelGlobal instanceof Panel) ) compReNamer.text = "Comp Find and Replace"; 
-    compReNamer.orientation = "column"; 
-    compReNamer.alignChildren = ["center","top"]; 
-    compReNamer.spacing = 10; 
-    compReNamer.margins = 16; 
+    // COMPRENAMER
+    // ===========
+    var compReNamer = (panelGlobal instanceof Panel) ? panelGlobal : new Window("palette", undefined, undefined, {borderless: false, resizeable: true});
+    if (!(panelGlobal instanceof Panel)) compReNamer.text = "Comp Find and Replace";
+    compReNamer.orientation = "column";
+    compReNamer.alignChildren = ["fill", "top"];
+    compReNamer.margins = 20;
+    compReNamer.spacing = 10;
 
-// FROMGROUP
-// =========
-var fromGroup = compReNamer.add("group", undefined, {name: "fromGroup"}); 
-    fromGroup.orientation = "row"; 
-    fromGroup.alignChildren = ["left","center"]; 
-    fromGroup.spacing = 10; 
-    fromGroup.margins = 0; 
+    // FROMGROUP
+    // =========
+    var fromGroup = compReNamer.add("group", undefined, { name: "fromGroup" });
+    fromGroup.orientation = "row";
+    fromGroup.alignChildren = ["fill", "center"];
 
-var from = fromGroup.add("statictext", undefined, undefined, {name: "from"}); 
-    from.text = "From:"; 
+    var from = fromGroup.add("statictext", undefined, undefined, { name: "from" });
+    from.text = "From:";
+    from.alignment = ["left","top"]; 
 
-var fromEditText = fromGroup.add('edittext {properties: {name: "fromEditText"}}'); 
-    //fromEditText.text = "EditText"; 
-    fromEditText.preferredSize.width = 100;
+    var fromEditText = fromGroup.add('edittext {properties: {name: "fromEditText"}}');
 
-// TOGROUP
-// =======
-var toGroup = compReNamer.add("group", undefined, {name: "toGroup"}); 
-    toGroup.orientation = "row"; 
-    toGroup.alignChildren = ["left","center"]; 
-    toGroup.spacing = 10; 
-    toGroup.margins = 0; 
+    // TOGROUP
+    // =======
+    var toGroup = compReNamer.add("group", undefined, { name: "toGroup" });
+    toGroup.orientation = "row";
+    toGroup.alignChildren = ["fill", "center"];
 
-var to = toGroup.add("statictext", undefined, undefined, {name: "to"}); 
-    to.text = "To:"; 
-    to.preferredSize.width = 36; 
-    to.justify = "right"; 
+    var to = toGroup.add("statictext", undefined, undefined, { name: "to" });
+    to.text = "To:";
+    to.alignment = ["left","top"]; 
 
-var toEditText = toGroup.add('edittext {properties: {name: "toEditText"}}'); 
-    //toEditText.text = "EditText"; 
-    toEditText.preferredSize.width = 100;
-// COMPRENAMER
-// ===========
-var runButton = compReNamer.add("button", undefined, undefined, {name: "runButton"}); 
-    runButton.text = "Run"; 
-    runButton.preferredSize.width = 110; 
-    runButton.onClick = function(){main()}
+    var toEditText = toGroup.add('edittext {properties: {name: "toEditText"}}');
 
-compReNamer.layout.layout(true);
-compReNamer.layout.resize();
-compReNamer.onResizing = compReNamer.onResize = function () { this.layout.resize(); }
+    // COMPRENAMER
+    // ===========
+    var runButtonGroup = compReNamer.add("group")
+    runButtonGroup.margins = 10;
+    var runButton = runButtonGroup.add("button", undefined, undefined, { name: "runButton" });
+    runButton.alignment = ["center", "top"];
+    runButton.text = "Run";
+    runButton.onClick = function () { main() }
 
-if ( compReNamer instanceof Window ) compReNamer.show();
+    compReNamer.layout.layout(true);
+    compReNamer.layout.resize();
+    compReNamer.onResizing = compReNamer.onResize = function () { this.layout.resize(); }
 
+    if (compReNamer instanceof Window) compReNamer.show();
 
+    function main() {
 
-function main(){
+        app.beginUndoGroup("Comp Rename");
 
-    app.beginUndoGroup("Comp Rename");
+        var items = app.project.items;
+        var toChange = [];
+        var from = fromEditText.text;
+        var to = toEditText.text;
 
-    var items = app.project.items;
-    var toChange = [];
-    var from = fromEditText.text;
-    var to = toEditText.text;
-    
-    for(i = 1; i <= items.length; i++){
-            if(items[i].name.indexOf(from) != -1 && items[i].typeName == "Composition" && items[i].selected == true){
-                        toChange.push(items[i]);
-                    }
+        for (i = 1; i <= items.length; i++) {
+            if (items[i].name.indexOf(from) != -1 && items[i].typeName == "Composition" && items[i].selected == true) {
+                toChange.push(items[i]);
             }
-    if(toChange.length > 0){
-            for(i = 0; i < toChange.length; i++){
-                    toChange[i].name = toChange[i].name.replace(from, to);
-                }
-        }else alert("No Compositions with the name \""+from+"\" found ")
+        }
+        if (toChange.length > 0) {
+            for (i = 0; i < toChange.length; i++) {
+                toChange[i].name = toChange[i].name.replace(from, to);
+            }
+        } else alert("No Compositions with the name \"" + from + "\" found ")
 
-    app.endUndoGroup();
-}
+        app.endUndoGroup();
+    }
+
+    return palette;
+
+}());
